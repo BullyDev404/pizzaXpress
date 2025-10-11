67f0f129bf5af2e2049d9c62bbb26effc4340b38
Peace <devbypeace@gmail.com>
2025-10-11 07:45:46 +0100

    feat: add AppLayout as parent route to manage shared UI

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <Link to= '/'> PizzaXpress </Link>
+            </header>
+        </div>
+    )
+}
+
+export default Header
